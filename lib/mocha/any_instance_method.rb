2095519f3ce0bff1b88ce03510f38d1e7861cc8a
Chris Roos <chris@seagul.co.uk>
2016-09-27 18:10:36 +0100

    Remove AnyInstanceMethod#remove_new_method
    Now that it's identical to the same method in its parent
    (`ClassMethod`).

--- lib/mocha/any_instance_method.rb
+++ lib/mocha/any_instance_method.rb
@@ -13,10 +13,6 @@ module Mocha
       stubbee.any_instance.reset_mocha
     end
 
-    def remove_new_method
-      definition_target.send(:remove_method, method)
-    end
-
     def restore_original_method
       unless RUBY_V2_PLUS
         if @original_method && @original_method.owner == stubbee
