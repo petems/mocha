8397c0c91aed33d1c82a08e0a99e495f356893ab
Chris Roos <chris@seagul.co.uk>
2016-09-28 12:01:42 +0100

    Encapsulate access to @original_method
    TODO: Explain this commit. I initially tried lazily returning
    `original_method` but that meant that it could potentially return the
    stubbed implementation depending on when it was called.

--- lib/mocha/class_method.rb
+++ lib/mocha/class_method.rb
@@ -40,7 +40,7 @@ module Mocha
     def hide_original_method
       if @original_visibility = method_visibility(method_name)
         begin
-          @original_method = original_method
+          store_original_method
           if RUBY_V2_PLUS
             use_prepended_module_for_stub_method
           elsif original_method_defined_on_stubbee?
@@ -67,12 +67,12 @@ module Mocha
       unless RUBY_V2_PLUS
         if original_method_defined_on_stubbee?
           if PRE_RUBY_V19
-            original_method = @original_method
+            local_original_method = original_method
             default_stub_method_owner.send(:define_method, method_name) do |*args, &block|
-              original_method.call(*args, &block)
+              local_original_method.call(*args, &block)
             end
           else
-            default_stub_method_owner.send(:define_method, method_name, @original_method)
+            default_stub_method_owner.send(:define_method, method_name, original_method)
           end
         end
         if @original_visibility
@@ -103,12 +103,14 @@ module Mocha
 
     private
 
-    def original_method
-      stubbee._method(method_name)
+    attr_reader :original_method
+
+    def store_original_method
+      @original_method = stubbee._method(method_name)
     end
 
     def original_method_defined_on_stubbee?
-      @original_method && @original_method.owner == default_stub_method_owner
+      original_method && original_method.owner == default_stub_method_owner
     end
 
     def remove_original_method_from_stubbee
